59a63f8762a65eb49410c30bab76e89234a928db
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-10-20 21:24:52 +0200

    try with input and send kafka to local topic

---
+++ kafka.js
@@ -0,0 +1,41 @@
+const { Kafka } = require('kafkajs');
+const program = require('commander');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const kafka = new Kafka({
+    clientId: 'test',
+    brokers: ['localhost:9092']
+})
+
+const run =  async () => {
+    const producer = kafka.producer()
+    
+    await producer.connect()
+    await producer.send({
+        topic: 'test',
+        messages: [
+            { value: "fuck dude" }
+        ]
+    });
+    
+    const consumer = kafka.consumer({groupId: 'test-group'})
+    
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test'})
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log("OUTPUT: ", {
+                value: message.value.toString()
+            });
+            
+        }
+    })
+
+    await consumer.stop();
+}
+
+run();
